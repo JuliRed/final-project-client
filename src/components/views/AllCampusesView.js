14bf70657aaf9e1f1a4d0fd9f83b552a2a6b2459
Drew Bruck <drewtbruck@gmail.com>
2024-05-17 04:39:52 -0400

    moved location of add campus button to top right of page, other minor UI fixes

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -8,14 +8,14 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const AllCampusesView = (props) => {
-  // If there is no campus, display a message.
+  // If there are no campuses, display a message.
   if (!props.allCampuses.length) {
     return (
       <div>
         <h3>There are no campuses. Tough luck, kid.</h3>
         <br/><br/>
-        <Link to={`/newcampus`}>
-        <button>Add New Campus</button>
+        <Link to={`/newcampus`} style={{ position: 'absolute', top: '100px', right: '40px' }}>
+          <button>Add New Campus</button>
         </Link>
       </div>
     )
@@ -25,6 +25,10 @@ const AllCampusesView = (props) => {
   return (
     <div>
       <h1>All Campuses</h1>
+      <Link to={`/newCampus`} style={{ position: 'absolute', top: '100px', right: '40px' }}>
+        <button>Add New Campus</button>
+      </Link>
+      <br/><br/>
 
       {props.allCampuses.map((campus) => (
         <div key={campus.id}>
@@ -36,16 +40,10 @@ const AllCampusesView = (props) => {
           <p>{campus.address}</p>
           <p>{campus.description}</p>
           <button onClick={() => props.deleteCampus(campus.id)}>Delete Campus</button>
-          <Link to={`/editcampus/${campus.id}`}><button>Edit Campus</button>
-          </Link>
+          <Link to={`/editcampus/${campus.id}`}><button>Edit Campus</button></Link>
           <hr/>
         </div>
       ))}
-      <br/>
-      <Link to={`/newCampus`}>
-        <button>Add New Campus</button>
-      </Link>
-      <br/><br/>
     </div>
   );
 };
